4327a95d6a08a158d013ddf961c85270711ed0ab
Arman Awan <armandoesstuff@gmail.com>
2024-04-09 18:59:47 +0100

    First Commit

---
+++ Utilities/Editor/LibraryEditor.cs
@@ -0,0 +1,31 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class LibraryEditor
+{
+    [MenuItem("Arman Library/Anchors to Corners %[")]
+    static void Shortcut_AnchorToCorner()
+    {
+        foreach (Transform t in Selection.transforms)
+        {
+            RectTransform rt = t.GetComponent<RectTransform>();
+            if (rt != null)
+            {
+                rt.AnchorToCorner();
+            }
+        }
+    }
+
+    [MenuItem("Arman Library/Corners to Anchors %]")]
+    static void Shortcut_CornerToAnchor()
+    {
+        foreach (Transform t in Selection.transforms)
+        {
+            RectTransform rt = t.GetComponent<RectTransform>();
+            if (rt != null)
+            {
+                rt.CornerToAnchor();
+            }
+        }
+    }
+}
