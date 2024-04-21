21ee0f0f208470eea3febc6358ef92189d6bdcfd
Arman Awan <armandoesstuff@gmail.com>
2024-04-21 12:49:33 +0100

    Assembly fixes

--- Misc/UserInterface/Editor/ButtonExtendedEditor.cs
+++ Misc/UserInterface/Editor/ButtonExtendedEditor.cs
@@ -12,7 +12,7 @@ namespace Armony.Misc.UserInterface.Editor
     public class ButtonExtendedEditor : ButtonEditor
     {
         private SerializedProperty[] m_serializedProperties;
-    
+
         private IEnumerable<FieldInfo> GetAllFields()
         {
             List<FieldInfo> fieldsList = new();
@@ -20,6 +20,7 @@ namespace Armony.Misc.UserInterface.Editor
 
             while (type != typeof(ButtonExtended).BaseType)
             {
+                // ReSharper disable once PossibleNullReferenceException
                 FieldInfo[] fields = type.GetFields(BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.DeclaredOnly);
                 fieldsList.InsertRange(0, fields);
                 type = type.BaseType;
@@ -27,35 +28,29 @@ namespace Armony.Misc.UserInterface.Editor
 
             return fieldsList.ToArray();
         }
-        
+
         protected override void OnEnable()
         {
             base.OnEnable();
             serializedObject.Update();
 
-            m_serializedProperties = (from field in GetAllFields() 
-                where field.IsPublic || field.GetCustomAttributes(typeof(SerializeField), true).Length > 0 
-                select serializedObject.FindProperty(field.Name) into serializedProperty 
-                where serializedProperty != null 
+            m_serializedProperties = (from field in GetAllFields()
+                where field.IsPublic || field.GetCustomAttributes(typeof(SerializeField), true).Length > 0
+                select serializedObject.FindProperty(field.Name)
+                into serializedProperty
+                where serializedProperty != null
                 select serializedProperty).ToArray();
         }
 
         public override void OnInspectorGUI()
         {
             base.OnInspectorGUI();
-            // Update serialized object
             serializedObject.Update();
-
-            // Draw default properties
-            //DrawDefaultInspector();
-
-            // Draw custom properties at the bottom
             foreach (SerializedProperty property in m_serializedProperties)
             {
                 EditorGUILayout.PropertyField(property);
             }
 
-            // Apply changes to serialized object
             serializedObject.ApplyModifiedProperties();
         }
     }
