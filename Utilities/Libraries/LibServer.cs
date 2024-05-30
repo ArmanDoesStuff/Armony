a2158dca16652a5066f319d75ee2cc8c70e19f79
Arman Awan <armandoesstuff@gmail.com>
2024-05-30 11:45:04 +0100

    Added LibServer

---
+++ Utilities/Libraries/LibServer.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using Unity.Netcode;
+
+namespace Armony.Utilities.Libraries
+{
+    public static class LibServer
+    {
+        public static ClientRpcParams SendExceptCaller(ServerRpcParams rpcParams)
+        {
+            List<ulong> clientIds = new(NetworkManager.Singleton.ConnectedClientsIds);
+            clientIds.Remove(rpcParams.Receive.SenderClientId);
+            ClientRpcParams clientRpcParams = new()
+            {
+                Send = new ClientRpcSendParams()
+                {
+                    TargetClientIds = clientIds
+                }
+            };
+            return clientRpcParams;
+        }
+        
+        public static ClientRpcParams SendCaller(ServerRpcParams rpcParams)
+        {
+            ClientRpcParams clientRpcParams = new()
+            {
+                Send = new ClientRpcSendParams()
+                {
+                    TargetClientIds = new[]{rpcParams.Receive.SenderClientId}
+                }
+            };
+            return clientRpcParams;
+        }
+    }
+}
