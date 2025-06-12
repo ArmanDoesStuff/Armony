275e049265c9db7d1ab6df1b6f8ef2fd2ca08fc7
Arman Awan <armandoesstuff@gmail.com>
2025-06-13 00:05:21 +0100

    Added oscillate

--- Scripts/Misc/ConstantOscillate.cs
+++ Scripts/Misc/ConstantOscillate.cs
@@ -1,30 +1,29 @@
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace Armony.Misc
 {
     public class ConstantOscillate : MonoBehaviour
     {
         [SerializeField]
-        private Vector3 m_offset;
-        private Vector3 Offset => m_offset;
+        private Vector3 offset;
         
         [SerializeField]
-        private float m_timeTaken = 1f;
-        private float TimeTaken => m_timeTaken;
-        
-        private Vector3 InitialPosition { get; set; }
-        private float Timer { get; set; }
+        private float timeTaken = 1f;
+
+        private Vector3 initialPosition;
+        private float timer;
 
         private void Start()
         {
-            InitialPosition = transform.localPosition;
-            Timer += Random.Range(0f, 1f); //Start randomly along path
+            initialPosition = transform.localPosition;
+            timer += Random.Range(0f, 1f);
         }
 
         private void Update()
         {
-            Timer += Time.deltaTime / TimeTaken;
-            transform.localPosition = InitialPosition + Offset * Mathf.Sin(Timer);
+            timer += Time.deltaTime / timeTaken;
+            transform.localPosition = initialPosition + offset * Mathf.Sin(timer);
         }
     }
 }
---
+++ Scripts/Misc/UserInterface/ConstantScale.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace Armony.Misc.UserInterface
+{
+    public class ConstantScale : MonoBehaviour
+    {
+        [SerializeField]
+        private Vector3 offset;
+        
+        [SerializeField]
+        private float timeTaken = 1f;
+
+        private Vector3 initialScale;
+        private float timer;
+
+        private void Start()
+        {
+            initialScale = transform.localScale;
+        }
+
+        private void Update()
+        {
+            timer += Time.deltaTime / timeTaken;
+            transform.localScale = initialScale + offset * Mathf.Sin(timer);
+        }
+    }
+}
