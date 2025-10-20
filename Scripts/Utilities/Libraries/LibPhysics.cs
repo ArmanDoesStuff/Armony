14b926a61b06965a1b489a02fd8494f713b31ec1
ArmanDoesStuff <armandoesstuff@gmail.com>
2025-10-20 23:57:11 +0100

    added physics lib

---
+++ Scripts/Utilities/Libraries/LibPhysics.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace Armony.Utilities.Libraries
+{
+    namespace Armony.Utilities.Libraries
+    {
+        public static class LibPhysics
+        {
+            public static Vector3 GetGroundPoint(this Vector3 _startPosition, int _layerMask = Physics.DefaultRaycastLayers) =>
+                Physics.Raycast(_startPosition, Vector3.down, out RaycastHit hit, Mathf.Infinity, _layerMask, QueryTriggerInteraction.Ignore) ? hit.point : _startPosition;
+        }
+    }
+}
